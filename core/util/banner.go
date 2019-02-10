dc6be95f449879b737c17c1f125245d0a323dfdf
zerjioang <sergio.anguita@opendeusto.es>
2019-02-10 20:39:40 +0100

    build commit added to docker image
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

--- core/util/banner.go
+++ core/util/banner.go
@@ -24,6 +24,7 @@ goroot            : $GOROOT
 goversion         : $GO_VERSION
 gocompiler        : $GO_COMPILER
 etherniti version : $VER
+etherniti commit  : $COMMIT
 `
 	version = "0.0.1"
 )
@@ -34,19 +35,24 @@ var (
 	Commit = ""
 )
 
+// thread safe init function
 func init() {
 	banner = getBannerFromTemplate()
 }
+
+// return welcome banner asci art message
 func WelcomeBanner() string {
+	banner = strings.Replace(banner, "$COMMIT", Commit, 1)
 	return banner
 }
+
+// inject runtime variables in welcome banner message
 func getBannerFromTemplate() string {
-	banner = strings.Replace(bannerTemplate, "$GOARCH", runtime.GOARCH, -1)
-	banner = strings.Replace(banner, "$GOARCH", runtime.GOOS, -1)
-	banner = strings.Replace(banner, "$GOROOT", runtime.GOROOT(), -1)
-	banner = strings.Replace(banner, "$GO_VERSION", runtime.Version(), -1)
-	banner = strings.Replace(banner, "$GO_COMPILER", runtime.Compiler, -1)
-	banner = strings.Replace(banner, "$VER", version, -1)
-	banner = strings.Replace(banner, "COMMIT", Commit, -1)
+	banner = strings.Replace(bannerTemplate, "$GOARCH", runtime.GOARCH, 1)
+	banner = strings.Replace(banner, "$GOARCH", runtime.GOOS, 1)
+	banner = strings.Replace(banner, "$GOROOT", runtime.GOROOT(), 1)
+	banner = strings.Replace(banner, "$GO_VERSION", runtime.Version(), 1)
+	banner = strings.Replace(banner, "$GO_COMPILER", runtime.Compiler, 1)
+	banner = strings.Replace(banner, "$VER", version, 1)
 	return banner
 }
