74f4f06f09f052b6c682b6b01799a3d06d4fab7c
zerjioang <sergio.anguita@opendeusto.es>
2019-02-14 17:54:16 +0100

    tunning image for smaller size
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ scripts/docker/hardening.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -x #trace on
+set -e #break on error
+echo "hardening container..."
+
+# docker image hardening
+sed -i -r "s/^$USER:!:/$USER:x:/" /etc/shadow
+
+# remove unnecessary user accounts.
+sed -i -r "/^($USER|root|appgroup)/!d" /etc/group
+sed -i -r "/^($USER|root|appgroup)/!d" /etc/passwd
+sed -i -r "/^($USER|root|appgroup)/!d" /etc/shadow
+
+# reinforce access permissions
+sysdirs="
+/bin
+/etc
+/lib
+/sbin
+/usr
+"
+find $sysdirs -xdev -type d \
+-exec chown root:root {} \; \
+-exec chmod 0755 {} \;
+
