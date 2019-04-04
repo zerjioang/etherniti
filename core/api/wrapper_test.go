1b313059c5449c4716ee77457314fc8638725f25
zerjioang <sergio.anguita@opendeusto.es>
2019-04-04 15:07:53 +0200

    added gotests script
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ core/api/wrapper_test.go
@@ -0,0 +1,12 @@
+// Copyright etherniti
+// SPDX-License-Identifier: Apache License 2.0
+
+package api
+
+import (
+	"testing"
+)
+
+func TestWrapper(t *testing.T){
+
+}
---
+++ scripts/gotests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Copyright etherniti
+# SPDX-License-Identifier: Apache License 2.0
+#
+
+cd "$(dirname "$0")"
+
+# move to project root dir from ./scripts to ./
+cd ..
+
+echo "Generating test functions base with gotests"
+
+echo "checking if gotests is installed in $GOPATH"
+if [[ ! -f ${GOPATH}/bin/gotests ]]; then
+	#statements
+	echo "gotests not found. Downloading via go get"
+	go get -u github.com/cweill/gotests
+fi
+
+#get all files excluding vendors
+filelist=$(find . -type f -name "*.go" | grep -vendor)
+for file in ${filelist}
+do
+	echo "generating gotests for file $file"
+	${GOPATH}/bin/gotests -all ${file}
+done
+
+echo "Code formatting done!"
