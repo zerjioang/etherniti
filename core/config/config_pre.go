8df338c506d5fa50a402edae2f670cc169578f6a
zerjioang <sergio.anguita@opendeusto.es>
2019-04-07 15:43:06 +0200

    released v0.0.4
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

--- core/config/config_pre.go
+++ core/config/config_pre.go
@@ -8,11 +8,11 @@
 package config
 
 import (
+	"github.com/zerjioang/etherniti/core/util/str"
 	"time"
 
 	"github.com/labstack/gommon/log"
 	"github.com/zerjioang/etherniti/core/eth/fastime"
-	"github.com/zerjioang/etherniti/core/util"
 )
 
 // openssl genrsa -out server.key 2048
@@ -109,9 +109,9 @@ var (
 
 func init() {
 	//hardcoded cert content as bytes
-	certPemBytes = util.Bytes(certPem)
+	certPemBytes = str.UnsafeBytes(certPem)
 	//hardcoded key content as bytes
-	keyPemBytes = util.Bytes(keyPem)
+	keyPemBytes = str.UnsafeBytes(keyPem)
 }
 
 //set default environment variables value for current context
