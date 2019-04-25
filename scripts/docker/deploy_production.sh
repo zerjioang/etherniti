de6f82d42a2fca17e0452bd53046df4cc7759d61
zerjioang <sergio.anguita@opendeusto.es>
2019-04-26 01:23:32 +0200

    added deployment script
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ scripts/docker/deploy_production.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Copyright Etherniti Project. All Rights Reserved.
+# SPDX-License-Identifier: GNU GPL v3
+#
+
+#source ./colors.sh
+
+echo "downloading etherniti/proxy:develop docker image"
+docker pull etherniti/proxy:develop
+
+echo "stopping previous proxy containers..."
+docker stop api && docker rm api
+
+echo "deploying proxy container"
+docker run \
+        -d \
+        -t \
+        -p 80:8080 \
+        -p 443:4430 \
+        --cpus=2 \
+        --hostname apollo \
+        --memory=2gb \
+        --restart unless-stopped \
+        --name api \
+        --log-opt max-size=20m \
+        --log-opt max-file=5 \
+        --log-opt labels=production_status \
+        -v $(pwd)/volumes/data:/home/etherniti/.etherniti \
+        etherniti/proxy:develop
+
+echo "etherniti proxy container deployed"
