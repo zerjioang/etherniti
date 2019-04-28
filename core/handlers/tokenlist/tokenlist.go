8138635b9bc7c79aa263fa97248459a1de7ae168
zerjioang <sergio.anguita@opendeusto.es>
2019-04-28 23:03:02 +0200

    released 0.0.8
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ core/handlers/tokenlist/tokenlist.go
@@ -0,0 +1,9 @@
+// Copyright etherniti
+// SPDX-License-Identifier: Apache License 2.0
+
+package tokenlist
+
+func init() {
+	// token data is loaded from json file downloaded from
+	// https://raw.githubusercontent.com/kvhnuke/etherwallet/mercury/app/scripts/tokens/ethTokens.json
+}
