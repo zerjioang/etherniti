7b739e4572b6740895cf65492f84a78e10afd04a
zerjioang <sergio.anguita@opendeusto.es>
2019-05-08 18:05:07 +0200

    removed old client cache functions
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

--- core/handlers/clientcache/clientcache.go
+++ core/handlers/clientcache/clientcache.go
@@ -3,23 +3,8 @@
 
 package clientcache
 
-import (
-	"github.com/zerjioang/etherniti/shared/protocol"
-	"github.com/zerjioang/etherniti/thirdparty/echo"
-)
-
 const (
 	CacheOneDay   = 86400    // seconds in a day
 	CacheInfinite = 31104000 // 86400 * 30 *12 // seconds in a year
 	NoExpiration  = -1
 )
-
-func Cached(c *echo.Context, cacheHit bool, seconds uint) (int, *echo.Context) {
-	return protocol.StatusOK, c
-}
-
-func CachedJsonBlob(c *echo.Context, cacheHit bool, seconds uint, data []byte) error {
-	var code int
-	code, c = Cached(c, cacheHit, seconds)
-	return c.JSONBlob(code, data)
-}
