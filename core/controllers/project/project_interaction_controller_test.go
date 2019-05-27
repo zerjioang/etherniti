97900e3b79731f77af81a9278b9f9e96486d6694
zerjioang <sergio.anguita@opendeusto.es>
2019-05-27 17:27:27 +0200

    updated project interaction controller
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ core/controllers/project/project_interaction_controller_test.go
@@ -0,0 +1,23 @@
+package project
+
+import (
+	"testing"
+
+	"github.com/zerjioang/etherniti/core/listener/common"
+	"github.com/zerjioang/etherniti/thirdparty/echo"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProjectController(t *testing.T) {
+	t.Run("create-project-controller", func(t *testing.T) {
+		pc := NewProjectController()
+		assert.NotNil(t, pc)
+	})
+	t.Run("create-project-write", func(t *testing.T) {
+		pc := NewProjectController()
+		assert.NotNil(t, pc)
+		err := pc.Create(common.NewContext(echo.New()))
+		assert.Nil(t, err)
+	})
+}
