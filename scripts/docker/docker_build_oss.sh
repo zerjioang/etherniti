ee7ee058fd6b3832c5a79d1f0109b812358fdb52
zerjioang <sergio.anguita@opendeusto.es>
2019-06-06 21:10:15 +0200

    oss docker image generation script updated
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

--- scripts/docker/docker_build_oss.sh
+++ scripts/docker/docker_build_oss.sh
@@ -21,9 +21,10 @@ source ./scripts/docker/docker_version.sh
 # load colored logs
 source ./scripts/colors.sh
 
-log "building Etherniti OSS"
+log "Etherniti Proxy OSS ($BUILD_VERSION, $edition, $mode) docker image"
 #our proxy configuration
 edition="oss"
+mode="pre"
 # get current os system architecture
 osarch=$(arch)
 # download docker image
@@ -31,18 +32,26 @@ log "creating docker image with following configuration:"
 echo "architecture : $osarch"
 echo "edition      : $edition"
 echo "version      : $BUILD_VERSION"
+echo "mode         : $mode"
 echo ""
 log "generating docker image etherniti/proxy-oss:$osarch-$BUILD_VERSION"
 echo ""
 
-build --build-arg BUILD_MODE=pre --build-arg BUILD_EDITION=oss -t etherniti/proxy-oss:$osarch-$BUILD_VERSION && \
+log "starting docker image compilation"
+build --build-arg BUILD_MODE=$mode --build-arg BUILD_EDITION=$edition -t etherniti/proxy-oss:$osarch-$BUILD_VERSION && \
+log "docker image compilation finished"
 
 # show message: Press any key to continue
 log "uploading to public docker hub"
 read -n 1 -s -r -p "Press any key to continue"
+echo ""
 
-docker push etherniti/proxy-oss:$osarch-$BUILD_VERSION
 # retag
-docker tag etherniti/proxy-oss:$osarch-$version etherniti/proxy-oss:$osarch-latest
-docker push etherniti/proxy-oss:$osarch-$version
+log "creating latest docker image from current version ($BUILD_VERSION)"
+docker tag etherniti/proxy-oss:$osarch-$BUILD_VERSION etherniti/proxy-oss:$osarch-latest
+# push
+log "uploading version $BUILD_VERSION to dockerhub"
+docker push etherniti/proxy-oss:$osarch-$BUILD_VERSION
+log "uploading version 'latest' to dockerhub"
 docker push etherniti/proxy-oss:$osarch-latest
+ok "Etherniti Proxy OSS ($BUILD_VERSION, $edition, $mode) docker image successfully uploaded"
