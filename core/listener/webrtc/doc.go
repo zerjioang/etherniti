b6870b22eccc086a99e2f9de59ec5ca6b02ec14a
zerjioang <sergio.anguita@opendeusto.es>
2019-06-24 15:43:05 +0200

    ignored some sensitive files
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

---
+++ core/listener/webrtc/doc.go
@@ -0,0 +1,10 @@
+// +build ignore
+package webrtc
+
+/*
+WebRTC (Web Real-Time Communication) is a free, open-source project that provides
+web browsers and mobile applications with real-time communication (RTC) via simple
+application programming interfaces (APIs). It allows audio and video communication
+to work inside web pages by allowing direct peer-to-peer communication, eliminating
+the need to install plugins or download native apps
+ */
