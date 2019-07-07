5dc5c42dd8b80581cbc51a9837bb00e99c460259
zerjioang <sergio.anguita@opendeusto.es>
2019-07-08 01:14:38 +0200

    added runtime benchmark score calculation for server performance evaluation
    Signed-off-by: zerjioang <sergio.anguita@opendeusto.es>

--- core/bench/pi.go
+++ core/bench/pi.go
@@ -1,1 +1,71 @@
 package bench
+
+import (
+	"github.com/zerjioang/etherniti/core/logger"
+	"github.com/zerjioang/etherniti/core/modules/monotonic"
+	"math"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+)
+
+var (
+	// runtime benchmark execution score
+	runScore int64
+)
+
+func CalculateScore(){
+	logger.Debug("benchmarking current server configuration")
+	logger.Debug("getting benchmark (multicore) score")
+	cores := runtime.NumCPU()
+	runtime.GOMAXPROCS(cores)
+
+	var wait sync.WaitGroup
+
+	counts := make([]int, cores)
+
+	// 30 milion samples
+	samples := 30000000
+
+	start := monotonic.Now()
+	wait.Add(cores)
+
+	for i := 0; i < cores; i++ {
+		go MonteCarlo(100.0, samples/cores, &counts[i], &wait)
+	}
+
+	wait.Wait()
+
+	total := 0
+	for i := 0; i < cores; i++ {
+		total += counts[i]
+	}
+
+	//pi := (float64(total) / float64(samples)) * 4
+	totalt := monotonic.Since(start)
+	score := float64(samples)/totalt.Seconds()
+	runScore = int64(score)
+}
+
+func GetScore() int64 {
+	return runScore
+}
+
+func MonteCarlo(radius float64, reps int, result *int, wait *sync.WaitGroup) {
+	var x, y float64
+	count := 0
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	for i := 0; i < reps; i++ {
+		x = random.Float64() * radius
+		y = random.Float64() * radius
+
+		if num := math.Sqrt(x*x + y*y); num < radius {
+			count++
+		}
+	}
+	*result = count
+	wait.Done()
+}
--- core/bench/pi_test.go
+++ core/bench/pi_test.go
@@ -1,1 +1,45 @@
 package bench
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+
+	"github.com/zerjioang/etherniti/core/modules/monotonic"
+)
+
+func TestMonteCarlo(t *testing.T) {
+	cores := runtime.NumCPU()
+	runtime.GOMAXPROCS(cores)
+
+	var wait sync.WaitGroup
+
+	counts := make([]int, cores)
+
+	// 10 milion samples
+	samples := 10000000
+
+	start := monotonic.Now()
+	wait.Add(cores)
+
+	for i := 0; i < cores; i++ {
+		go MonteCarlo(100.0, samples/cores, &counts[i], &wait)
+	}
+
+	wait.Wait()
+
+	total := 0
+	for i := 0; i < cores; i++ {
+		total += counts[i]
+	}
+
+	pi := (float64(total) / float64(samples)) * 4
+	totalt := monotonic.Since(start)
+	score := float64(samples)/totalt.Seconds()
+
+	fmt.Println("Time: ", totalt)
+	fmt.Println("Score: ", int64(score))
+	fmt.Println("pi: ", pi)
+	fmt.Println("")
+}
